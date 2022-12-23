64b39d3c09992dcb4557d3d2b0a5971df2dddd64
Tram Nguyen <tramngoc1402@gmail.com>
2022-12-23 18:10:46 +0200

    update gitignore

---
+++ src/data/contactData.js
@@ -0,0 +1,24 @@
+const contactData = [
+  {
+    name: 'email',
+    faIcon: 'fa-solid fa-envelope',
+    url: 'mailto:tramngoc1402@gmail.com?Subject=From%20your%20portfolio',
+  },
+  {
+    name: 'phone',
+    faIcon: 'fa-solid fa-phone',
+    url: 'tel:+358449513040',
+  },
+  {
+    name: 'github',
+    faIcon: 'fa-brands fa-github',
+    url: 'https://github.com/tramnguyenhere',
+  },
+  {
+    name: 'linkedin',
+    faIcon: 'fa-brands fa-linkedin',
+    url: 'https://www.linkedin.com/in/tramnguyenhere/',
+  },
+];
+
+export default contactData;
